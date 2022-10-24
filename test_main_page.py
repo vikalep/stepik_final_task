cd7f35db0c2ef73a844e864123807e2946927486
vlepeshko <vlepeshko@vlepeshkos-MacBook-Pro.local>
2022-10-24 22:54:36 +0300

    updated test to check is login link presented

--- test_main_page.py
+++ test_main_page.py
@@ -1,9 +1,9 @@
 import pytest
-from pages.main_page import MainPage
+from .pages.main_page import MainPage
 
 
 def test_guest_can_go_to_login_page(browser):
     link = "http://selenium1py.pythonanywhere.com/"
     page = MainPage(browser, link)
     page.open()
-    page.go_to_login_page()
+    page.should_be_login_link()
