64580ed14a289c4f564153c66e30931570b26031
vlepeshko <vlepeshko@vlepeshkos-MacBook-Pro.local>
2022-10-27 23:05:41 +0300

    update main tests with new login tests

--- test_main_page.py
+++ test_main_page.py
@@ -1,5 +1,6 @@
 import pytest
 from .pages.main_page import MainPage
+from .pages.login_page import LoginPage
 
 
 def test_guest_can_go_to_login_page(browser):
@@ -7,3 +8,22 @@ def test_guest_can_go_to_login_page(browser):
     page = MainPage(browser, link)
     page.open()
     page.should_be_login_link()
+
+def test_should_be_login_url(browser):
+    link = "http://selenium1py.pythonanywhere.com/en-gb/accounts/login/"
+    page = LoginPage(browser, link)
+    page.open() 
+    page.should_be_login_url()  
+
+def test_should_be_login_form(browser):
+    link = "http://selenium1py.pythonanywhere.com/en-gb/accounts/login/"
+    page = LoginPage(browser, link)
+    page.open()
+    page.should_be_login_form()
+
+def test_should_be_register_form(browser):
+    link = "http://selenium1py.pythonanywhere.com/en-gb/accounts/login/"
+    page = LoginPage(browser, link)
+    page.open()
+    page.should_be_register_form()
+
