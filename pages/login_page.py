6b7109f7bf7eeff35e0f397baef4c69ce98f1e41
vlepeshko <vlepeshko@vlepeshkos-MacBook-Pro.local>
2022-10-27 23:03:36 +0300

    Add login_page

---
+++ pages/login_page.py
@@ -0,0 +1,23 @@
+from .base_page import BasePage
+from .locators import LoginPageLocators
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+
+
+class LoginPage(BasePage):
+    def should_be_login_page(self):
+        self.should_be_login_url()
+        self.should_be_login_form()
+        self.should_be_register_form()
+
+    def should_be_login_url(self):
+        # реализуйте проверку на корректный url адрес
+        assert "login" in self.browser.current_url, "It's not a login page"
+
+    def should_be_login_form(self):
+        # реализуйте проверку, что есть форма логина
+        assert self.is_element_present(*LoginPageLocators.LOGIN_HEADER), "Log In header is not presented"
+
+    def should_be_register_form(self):
+        # реализуйте проверку, что есть форма регистрации на странице
+        assert self.is_element_present(*LoginPageLocators.REGISTER_HEADER), "Register header is not presented"
