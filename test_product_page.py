3d0fce38b9d56709ff2d3456865632523c20679f
vlepeshko <vlepeshko@vlepeshkos-MacBook-Pro.local>
2022-11-10 20:57:19 +0300

    add test product file

---
+++ test_product_page.py
@@ -0,0 +1,107 @@
+import pytest
+import time
+import math
+from .pages.main_page import MainPage
+from .pages.login_page import LoginPage
+from .pages.product_page import ProductPage
+from .pages.base_page import BasePage
+from .pages.basket_page import BasketPage
+
+@pytest.mark.register_new_user
+class TestUserAddToBasketFromProductPage():
+
+    @pytest.fixture(scope = "function", autouse = True)
+    def setup(self, browser):
+        link = "http://selenium1py.pythonanywhere.com/en-gb/accounts/login/"
+        page = LoginPage(browser, link)
+        page.open()
+        email = str(time.time()) +"@test.com"
+        password = "112358test"
+        page.register_new_user(email, password)
+        #page = BasePage(browser, browser.current_url)
+        page.should_be_authorized_user()
+
+    def test_user_cant_see_success_message(self, browser):
+        link = "http://selenium1py.pythonanywhere.com/catalogue/the-shellcoders-handbook_209/?promo=newYear"
+        page = ProductPage(browser, link)
+        page.open()
+        page.should_not_be_success_message()
+
+    @pytest.mark.need_review
+    def test_user_can_add_product_to_basket(self, browser):
+        link = "http://selenium1py.pythonanywhere.com/catalogue/the-shellcoders-handbook_209/?promo=newYear"
+        page = ProductPage(browser, link)
+        page.open()
+        page.add_product_to_buscket()
+        page.solve_quiz_and_get_code()
+        page.should_be_message_that_product_add_to_basket()
+        page.should_be_right_price()
+
+@pytest.mark.skip
+#@pytest.mark.parametrize("promo_num", ["0","1","2","3","4","5","6", pytest.param("7", marks = pytest.mark.xfail),"8","9"])
+def test_add_product_to_basket(browser, promo_num):
+    #link = "http://selenium1py.pythonanywhere.com/catalogue/the-shellcoders-handbook_209/?promo=newYear"
+    link = f"http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer{promo_num}"
+    page = ProductPage(browser, link)
+    page.open()
+    page.add_product_to_buscket()
+    page.solve_quiz_and_get_code()
+    page.should_be_message_that_product_add_to_basket()
+    page.should_be_right_price()
+
+@pytest.mark.xfail
+def test_guest_cant_see_success_message_after_adding_product_to_basket(browser):
+    link = "http://selenium1py.pythonanywhere.com/catalogue/the-shellcoders-handbook_209/?promo=newYear"
+    page = ProductPage(browser, link)
+    page.open()
+    page.add_product_to_buscket()
+    page.solve_quiz_and_get_code()
+    page.should_not_be_success_message()
+
+def test_guest_cant_see_success_message(browser):
+    link = "http://selenium1py.pythonanywhere.com/catalogue/the-shellcoders-handbook_209/?promo=newYear"
+    page = ProductPage(browser, link)
+    page.open()
+    page.should_not_be_success_message()
+
+@pytest.mark.xfail
+def test_message_disappeared_after_adding_product_to_basket(browser):
+    link = "http://selenium1py.pythonanywhere.com/catalogue/the-shellcoders-handbook_209/?promo=newYear"
+    page = ProductPage(browser, link)
+    page.open()
+    page.add_product_to_buscket()
+    page.solve_quiz_and_get_code()
+    page.should_not_be_success_message()
+
+def test_guest_should_see_login_link_on_product_page(browser):
+    link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-city-and-the-stars_95/"
+    page = ProductPage(browser, link)
+    page.open()
+    page.should_be_login_link()
+
+@pytest.mark.need_review
+def test_guest_can_go_to_login_page_from_product_page(browser):
+    link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-city-and-the-stars_95/"
+    page = ProductPage(browser, link)
+    page.open()
+    page.go_to_login_page()
+
+@pytest.mark.need_review
+def test_guest_can_add_product_to_basket(self, browser):
+        link = "http://selenium1py.pythonanywhere.com/catalogue/the-shellcoders-handbook_209/?promo=newYear"
+        page = ProductPage(browser, link)
+        page.open()
+        page.add_product_to_buscket()
+        page.solve_quiz_and_get_code()
+        page.should_be_message_that_product_add_to_basket()
+        page.should_be_right_price()
+
+@pytest.mark.need_review
+def test_guest_cant_see_product_in_basket_opened_from_product_page(browser):
+    link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-city-and-the-stars_95/"
+    page = BasketPage(browser, link)
+    page.open()
+    page.go_to_view_basket()
+    page.should_be_empty_basket()
+    page.items_shouldnt_be_in_basket()
+
