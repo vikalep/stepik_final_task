44aa0adf09c04010bf84f5e5d91576f851a5bfd2
vlepeshko <vlepeshko@vlepeshkos-MacBook-Pro.local>
2022-11-10 21:01:47 +0300

    update login_page file

--- pages/login_page.py
+++ pages/login_page.py
@@ -1,8 +1,7 @@
-from .base_page import BasePage
-from .locators import LoginPageLocators
 from selenium import webdriver
 from selenium.webdriver.common.by import By
-
+from .base_page import BasePage
+from .locators import LoginPageLocators
 
 class LoginPage(BasePage):
     def should_be_login_page(self):
@@ -16,8 +15,21 @@ class LoginPage(BasePage):
 
     def should_be_login_form(self):
         # реализуйте проверку, что есть форма логина
-        assert self.is_element_present(*LoginPageLocators.LOGIN_HEADER), "Log In header is not presented"
+        assert self.is_element_present(*LoginPageLocators.LOGIN_FORM), "Log In form is not presented"
 
     def should_be_register_form(self):
         # реализуйте проверку, что есть форма регистрации на странице
-        assert self.is_element_present(*LoginPageLocators.REGISTER_HEADER), "Register header is not presented"
+        assert self.is_element_present(*LoginPageLocators.REGISTER_FORM), "Register form is not presented"
+
+    def register_new_user(self, email, password):
+        email_input = self.browser.find_element(*LoginPageLocators.REGISTER_EMAIL)
+        email_input.send_keys(email)
+        password_input = self.browser.find_element(*LoginPageLocators.REGISTER_PASSWORD)
+        password_input.send_keys(password)
+        confirm_password_input = self.browser.find_element(*LoginPageLocators.REGISTER_CONFIRM_PASSWORD)
+        confirm_password_input.send_keys(password)
+        register_btn = self.browser.find_element(*LoginPageLocators.REGISTER_BTN)
+        register_btn.click()
+
+    def should_be_registered_user(self):
+        assert self.is_element_present(*BasePageLocators.USER_ICON), "User is not registered, but should"
