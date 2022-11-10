00b181e91de2e580ab6933ff4f9d689ee218a24b
vlepeshko <vlepeshko@vlepeshkos-MacBook-Pro.local>
2022-11-10 20:56:27 +0300

    update test main file

--- test_main_page.py
+++ test_main_page.py
@@ -1,29 +1,62 @@
 import pytest
 from .pages.main_page import MainPage
 from .pages.login_page import LoginPage
+from .pages.base_page import BasePage
+from .pages.product_page import ProductPage
+from .pages.basket_page import BasketPage
+import time
 
+@pytest.mark.login_guest
+class TestLoginFromMainPage():
 
-def test_guest_can_go_to_login_page(browser):
-    link = "http://selenium1py.pythonanywhere.com/"
-    page = MainPage(browser, link)
-    page.open()
-    page.should_be_login_link()
+    def test_guest_can_go_to_login_page(self, browser):
+        link = "http://selenium1py.pythonanywhere.com"
+        page = MainPage(browser, link)
+        page.open()
+        page.go_to_login_page()
+        page.should_be_login_link()
+        login_page = LoginPage(browser, browser.current_url)
+        login_page.should_be_login_page()
 
+    def test_guest_should_see_login_link(self, browser):
+        link = "http://selenium1py.pythonanywhere.com/"
+        page = MainPage(browser, link)
+        page.open()
+        page.should_be_login_link()
+
+@pytest.mark.skip
 def test_should_be_login_url(browser):
     link = "http://selenium1py.pythonanywhere.com/en-gb/accounts/login/"
     page = LoginPage(browser, link)
     page.open() 
     page.should_be_login_url()  
 
+@pytest.mark.skip
 def test_should_be_login_form(browser):
     link = "http://selenium1py.pythonanywhere.com/en-gb/accounts/login/"
     page = LoginPage(browser, link)
     page.open()
     page.should_be_login_form()
 
+@pytest.mark.skip
 def test_should_be_register_form(browser):
     link = "http://selenium1py.pythonanywhere.com/en-gb/accounts/login/"
     page = LoginPage(browser, link)
     page.open()
     page.should_be_register_form()
 
+def test_guest_cant_see_product_in_basket_opened_from_main_page(browser):
+    link = "http://selenium1py.pythonanywhere.com/"
+    page = BasketPage(browser, link)
+    page.open()
+    page.go_to_view_basket()
+    page.should_be_empty_basket()
+    page.items_shouldnt_be_in_basket()
+
+def test_guest_cant_see_product_in_basket_opened_from_product_page(browser):
+    link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-city-and-the-stars_95/"
+    page = BasketPage(browser, link)
+    page.open()
+    page.go_to_view_basket()
+    page.should_be_empty_basket()
+    page.items_shouldnt_be_in_basket()
