2187f3d9cce651667c560e2c775bdfb7d675aec6
vikalep <66069756+vikalep@users.noreply.github.com>
2022-11-11 22:14:39 +0300

    Update test_product_page.py

--- test_product_page.py
+++ test_product_page.py
@@ -40,8 +40,8 @@ class TestUserAddToBasketFromProductPage():
 @pytest.mark.skip
 #@pytest.mark.parametrize("promo_num", ["0","1","2","3","4","5","6", pytest.param("7", marks = pytest.mark.xfail),"8","9"])
 def test_add_product_to_basket(browser, promo_num):
-    #link = "http://selenium1py.pythonanywhere.com/catalogue/the-shellcoders-handbook_209/?promo=newYear"
-    link = f"http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer{promo_num}"
+    link = "http://selenium1py.pythonanywhere.com/catalogue/the-shellcoders-handbook_209/?promo=newYear"
+    #link = f"http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer{promo_num}"
     page = ProductPage(browser, link)
     page.open()
     page.add_product_to_buscket()
